fd9f4a4e600918b53c41ff1de87abc412dd6590a
GreedForge <jafecafe@gmail.com>
2017-08-25 17:14:30 -0500

    functionality done

--- public/app.js
+++ public/app.js
@@ -14,6 +14,14 @@ $.getJSON("/saved", function(data){
   }
 });
 
+$.getJSON("/articles", function(data) {
+  // For each one
+  for (var i = 0; i < data.length; i++) {
+    // Display the apropos information on the page
+    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title +"<button class='save' id='"+data[i]._id+"'>Save Article</button>"+ "<br />" + data[i].link + "</p>");
+  }
+});
+
 $(document).on("click", ".save", function() {
   var thisId = $(this).attr("id");
   $.ajax({
