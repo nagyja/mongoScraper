bc6b8ad60acfe5c1531409fe9177788b3b08455a
Mikael Walters <mikael.walters@gmail.com>
2017-08-27 11:21:44 -0500

    adding heroku mLab support

--- server.js
+++ server.js
@@ -29,7 +29,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
 // Database configuration with mongoose
-mongoose.connect("mongodb://localhost/mongoScraper");
+var databaseUri = "mongodb://localhost/mongoScraper";
+
+if(process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+} else {
+  mongoose.connect(databaseUri);
+}
+
 var db = mongoose.connection;
 
 // Show any mongoose errors
@@ -78,35 +85,6 @@ app.post("/saved:id", function(req, res) {
 
 
 // A GET request to scrape the echojs website
-// app.get("/scrape", function(req, res) {
-//   // First, we grab the body of the html with request
-//   request("http://www.echojs.com/", function(error, response, html) {
-//     // Then, we load that into cheerio and save it to $ for a shorthand selector
-//     var $ = cheerio.load(html);
-//     // Now, we grab every h2 within an article tag, and do the following:
-//     $("article h2").each(function(i, element) {
-//
-//       // Save an empty result object
-//       var result = {};
-//
-//       // Add the text and href of every link, and save them as properties of the result object
-//       result.title = $(this).children("a").text();
-//       result.link = $(this).children("a").attr("href");
-//
-//       // Using our Article model, create a new entry
-//       // This effectively passes the result object to the entry (and the title and link)
-//       var entry = new Article(result);
-//
-//       // Now, save that entry to the db
-//       entry.save(function(err, doc) {
-//         // Log any errors
-//         if (err) {
-//           console.log(err // Or log the doc
-//           );
-//         } else {
-
-
-// A GET request to scrape the echojs website
 app.get("/scrape", function(req, res) {
     // First, we grab the body of the html with request
     request("http://www.echojs.com/", function(error, response, html) {
@@ -139,14 +117,6 @@ app.get("/scrape", function(req, res) {
 
                 }
             });
-
-
-            // Tell the browser that we finished scraping the text
-            // res.send("scrape complete");
-            // res.redirect("/");
-
-            // Tell the browser that we finished scraping the text
-            // res.redirect("/");
 
         });
 
