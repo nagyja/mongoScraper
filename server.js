714ee00e89b00d85b7f6033cbb310ed810c0dc7f
Jeffrey Nagy <nagyja@gmail.com>
2017-08-27 19:26:05 -0500

    fixes

--- server.js
+++ server.js
@@ -83,12 +83,9 @@ app.post("/saved:id", function(req, res) {
                     }
                 });
         }
-
-      });
-    }
-  });
+    });
 });
-
+ 
 
 // Find all the saved articles
 app.get("/save", function(req, res) {
@@ -107,19 +104,14 @@ app.get("/save", function(req, res) {
 
 // A GET request to scrape the echojs website
 app.get("/scrape", function(req, res) {
-  // First, we grab the body of the html with request
-  request("http://www.echojs.com/", function(error, response, html) {
-    // Then, we load that into cheerio and save it to $ for a shorthand selector
-    var $ = cheerio.load(html);
-    // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
-
-
-  // A GET request to scrape the echojs website
-  app.get("/scrape", function(req, res) {
-
+    // First, we grab the body of the html with request
+    request("http://www.echojs.com/", function(error, response, html) {
+        // Then, we load that into cheerio and save it to $ for a shorthand selector
+        var $ = cheerio.load(html);
+        // Now, we grab every h2 within an article tag, and do the following:
+        $("article h2").each(function(i, element) {
+        });
     });
-
 });
 
 
